6c897088cb3bd10047dc70f9663676c9652f3900
Remzo00 <gusinacremzo00@gmail.com>
2023-10-19 19:17:33 +0200

    primitivni tipovi podataka

---
+++ prvi cas/app.js
@@ -0,0 +1,16 @@
+//sluzi nam za prikaz teksta ili rezultata u konzoli
+console.log("Hello world")
+
+// alert("Hello world")
+
+document.write("Hello world")
+
+//PROSTI TIPOVI PODATAKA
+
+//string -> prikaz teksta
+//number -> prikaz broja
+//boolean(true/false) -> odredjuje flow naseg programa (logicka operacija)
+//undefined -> sluzi za prikaz podataka koji nisu definisani
+//null -> odsustvo vrednosti
+
+var x = 5
