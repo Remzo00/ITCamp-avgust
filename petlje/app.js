df4f02a643a5199ce13a8d35ab8a454341d40a7e
Remzo00 <gusinacremzo00@gmail.com>
2023-11-04 19:47:08 +0100

    petlje

---
+++ petlje/app.js
@@ -0,0 +1,34 @@
+//Pronaci brojeve koji su deljivi sa 2. Petlja pocinje od 1 i zavrsava do 10
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log("parni", i);
+  } else {
+    console.log("neparni", i);
+  }
+}
+
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+
+// for (inicijalizacija; uslov; brojac){
+// rezultat
+// }
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Hello world", i);
+}
+
+for (let i = 10; i >= 1; i--) {
+  console.log("Hello world", i);
+}
+
+let i = 1;
+
+while (i <= 10) {
+  console.log("Hello world", i);
+  i++;
+}
