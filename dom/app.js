4d779a462b014b088db1fcd79522973b1741feba
Remzo00 <gusinacremzo00@gmail.com>
2023-12-30 18:51:44 +0100

    nodes

--- dom/app.js
+++ dom/app.js
@@ -11,54 +11,65 @@
 
 //getElementById -> Uzima element po njegovom ID-u
 
-const noviElement = document.getElementById("naslov");
-// const noviElement = document.querySelector(".naslov");
-const items = document.getElementsByClassName("list-group-fruit");
-items[1].style.backgroundColor = "red";
-items[0].innerText = "nesto";
-// items.style.color = "green";
+// const noviElement = document.getElementById("naslov");
+// // const noviElement = document.querySelector(".naslov");
+// const items = document.getElementsByClassName("list-group-fruit");
+// items[1].style.backgroundColor = "red";
+// items[0].innerText = "nesto";
+// // items.style.color = "green";
 
-for (let i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "red";
-  console.log(items);
-}
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "red";
+//   console.log(items);
+// }
 
-noviElement.style.color = "blue";
+// noviElement.style.color = "blue";
 
-function boja() {
-  noviElement.style.color = "red";
-}
+// function boja() {
+//   noviElement.style.color = "red";
+// }
 
-function validacija() {
-  const email = document.forms["myForm"]["email"];
-  const pass = document.forms["myForm"]["password"];
-  const confirm = document.forms["myForm"]["confirmPass"];
+// function validacija() {
+//   const email = document.forms["myForm"]["email"];
+//   const pass = document.forms["myForm"]["password"];
+//   const confirm = document.forms["myForm"]["confirmPass"];
 
-  if (email.value.length < 9) {
-    alert("Popuni email");
-    return false;
-  }
+//   if (email.value.length < 9) {
+//     alert("Popuni email");
+//     return false;
+//   }
 
-  if (pass.value.length < 6) {
-    alert("Password mora imati vise karaktera");
-    return false;
-  }
+//   if (pass.value.length < 6) {
+//     alert("Password mora imati vise karaktera");
+//     return false;
+//   }
 
-  if (confirm.value !== pass.value) {
-    alert("Sifre moraju biti iste");
-    return false;
-  }
-}
+//   if (confirm.value !== pass.value) {
+//     alert("Sifre moraju biti iste");
+//     return false;
+//   }
+// }
 
-//eventListener
-const modal = document.getElementById("modal");
-const modalButton = document.getElementById("modal-button");
-const modalCloseButton = document.querySelector(".close");
+// //eventListener
+// const modal = document.getElementById("modal");
+// const modalButton = document.getElementById("modal-button");
+// const modalCloseButton = document.querySelector(".close");
 
-modalButton.addEventListener("click", function () {
-  modal.style.display = "block";
-});
+// modalButton.addEventListener("click", function () {
+//   modal.style.display = "block";
+// });
 
-modalCloseButton.addEventListener("click", function () {
-  modal.style.display = "none";
-});
+// modalCloseButton.addEventListener("click", function () {
+//   modal.style.display = "none";
+// });
+
+//Nodes
+
+const paragraf = document.createElement("p");
+const tekst = document.createTextNode("Neki tekst");
+paragraf.appendChild(tekst);
+
+const div = document.createElement("div");
+const body = document.getElementById("body");
+body.appendChild(div);
+div.appendChild(paragraf);
