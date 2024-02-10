cf60feaadabc59c7fa56ed6da76e6dea5c24cad4
Remzo00 <gusinacremzo00@gmail.com>
2024-02-10 19:02:55 +0100

    Prikaz podataka sa API

--- advanced js/async js/app.js
+++ advanced js/async js/app.js
@@ -63,7 +63,16 @@ const getData = async () => {
   try {
     const response = await fetch("https://fakestoreapi.com/products?limit=5");
     const data = await response.json();
-    console.log(data);
+    const productList = document.getElementById("container");
+    data.forEach((item) => {
+      const productElements = document.createElement("div");
+      productElements.innerHTML = `
+        <h1>${item.title}</h1>
+        <h3>${item.price}</h3>
+        <img src="${item.image}">
+      `;
+      productList.appendChild(productElements);
+    });
   } catch (error) {
     console.log(error);
   }
