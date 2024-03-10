7a148514059d3522ba3440c33a36423d865650ce
Remzo00 <gusinacremzo00@gmail.com>
2024-03-10 22:16:29 +0100

    Book order

---
+++ OOP/Knjizara/app.js
@@ -0,0 +1,24 @@
+//KORAK 1
+//Definisati klasu Knjiga koja sadrži: naslov, autor, cena.
+//Klasa Knjiga ima metode: getInfo()
+//U metodi getInfo() prikazati informacije o knjizi.
+
+//KORAK 2
+//Definisati klasu User koja sadrži: ime, email.
+//Klasa User ce sadrzati korpu ali ne u konstruktoru, koja ce po defaultu biti postavljena kao prazan niz
+//Klasa User ima tri metode: addToCart(knjiga), viewCart(), createOrder().
+//U metodi addToCart(knjiga) dodati knjigu u korpu.
+//U metodi viewCart() prikazati informacije o korpi.
+//U metodi createOrder() kreirati narudžbinu i isprazniti korpu
+
+//KORAK 3
+//Napraviti nekoliko instanci klase Knjiga
+//Napraviti instancu klase User
+//Dodati par knjiga u korpu
+//Nakon dodavanja par knjiga, proveriti da li su knjige dodate u korpu
+//Nakon kreiranja narudzbine, isprazniti korpu
+
+//HINT
+//Obzirom da je korpa prazan NIZ, imaj u vidu da ces raditi sa nizovima.
+//Za dodavanje mozes koristiti push metodu da ubacis knjigu u niz.
+//Za prikaz svih knjiga u korpi mozes koristiti forEach() metodu jer su knijige nizovi.
